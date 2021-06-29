39b55c3e292b13996107ea3b5a9d9a2be3496578
袁其祥 <2269941675@qq.com>
2021-06-29 17:36:24 +0800

    Dev 分支提交的

---
+++ src/main/java/com/suning/test.java
@@ -0,0 +1,12 @@
+package com.suning;
+
+/**
+ * @author yqx
+ * @create 2021/6/29
+ * @Desc
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("分支");
+    }
+}
